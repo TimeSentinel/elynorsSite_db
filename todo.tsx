daa3f21456686009a72112a472e36dbba0aa7f79
TimeSentinel <lance.stubblefield@gmail.com>
2025-02-08 19:32:02 -0800

    todo list

--- todo.tsx
+++ todo.tsx
@@ -1,11 +1,16 @@
 /* ---------------------------------------
-/todo.tsx
+----- todo file -----
 PROJECT: elynors;
 
 (c) 2025 Lance Stubblefield
 
-todo: after adding to cart, go back to menu???
-todo: shopping cart display
+todo: after adding to cart, go back to menu???;
+todo: cart item quantity;
+todo: shopping cart display;
+todo: css color schemes;
+todo: meal pictures;
+
+
 
 
 
